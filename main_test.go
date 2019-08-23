cb0f89c5c7d25dd00957afaefe25a517578083b3
Fabian Simon <fabian.simon@hornbach.com>
2019-08-23 14:58:59 +0200

    Added tests

--- main_test.go
+++ main_test.go
@@ -1,31 +1,104 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
 
-func Test_getFilledValue(t *testing.T) {
+	"github.com/docker/docker/api/types"
+	"github.com/fasibio/funk_agent/tracker"
+)
+
+func getSwarmModeLabels(containername, servicename, namespace string) map[string]string {
+	res := make(map[string]string)
+	res["com.docker.swarm.task.name"] = containername
+	res["com.docker.swarm.service.name"] = servicename
+	res["com.docker.stack.namespace"] = namespace
+	return res
+}
+func Test_getFilledMessageAttributes(t *testing.T) {
 	tests := []struct {
-		name     string
-		value    string
-		fallback string
-		want     string
+		name    string
+		holder  Holder
+		tracker tracker.Tracker
+		want    Attributes
 	}{
 		{
-			name:     "Get Value",
-			value:    "value",
-			fallback: "ignored",
-			want:     "value",
+			name: "fill Attribute without Swarmmode",
+			want: Attributes{
+				Containername: "Containername",
+				Host:          "MockTest",
+				ContainerID:   "MockImageid",
+			},
+			holder: Holder{
+				itSelfNamedHost: "MockTest",
+				Props: Props{
+					SwarmMode: false,
+				},
+			},
+			tracker: tracker.Tracker{
+				Container: types.Container{
+					ImageID: "MockImageid",
+					Names: []string{
+						"Containername",
+					},
+				},
+			},
 		},
 		{
-			name:     "Get Fallback",
-			value:    "",
-			fallback: "fallback",
-			want:     "fallback",
+			name: "fill Attribute with Swarmmode all labels are set",
+			want: Attributes{
+				Containername: "Containername",
+				Servicename:   "ServiceName",
+				Namespace:     "namespace",
+				Host:          "MockTest",
+				ContainerID:   "MockImageid",
+			},
+			holder: Holder{
+				itSelfNamedHost: "MockTest",
+				Props: Props{
+					SwarmMode: true,
+				},
+			},
+			tracker: tracker.Tracker{
+				Container: types.Container{
+					Labels: getSwarmModeLabels("Containername", "ServiceName", "namespace"),
+					Names: []string{
+						"Containername_not_used",
+					},
+					ImageID: "MockImageid",
+				},
+			},
+		},
+		{
+			name: "fill Attribute with Swarmmode all labels without containername label are set he use the fallback container name",
+			want: Attributes{
+				Containername: "Containername",
+				Servicename:   "ServiceName",
+				Namespace:     "namespace",
+				Host:          "MockTest",
+				ContainerID:   "MockImageid",
+			},
+			holder: Holder{
+				itSelfNamedHost: "MockTest",
+				Props: Props{
+					SwarmMode: true,
+				},
+			},
+			tracker: tracker.Tracker{
+				Container: types.Container{
+					Labels: getSwarmModeLabels("", "ServiceName", "namespace"),
+					Names: []string{
+						"Containername",
+					},
+					ImageID: "MockImageid",
+				},
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := getFilledValue(tt.value, tt.fallback); got != tt.want {
-				t.Errorf("getFilledValue() = %v, want %v", got, tt.want)
+			if got := getFilledMessageAttributes(&tt.holder, &tt.tracker); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getFilledMessageAttributes() = %v, want %v", got, tt.want)
 			}
 		})
 	}
